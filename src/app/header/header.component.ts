c8e93a12fed9ade85e06a2eb1ae0ec336efb6ed1
Rafael Zasas <zasas.rafi@gmail.com>
2020-05-25 15:46:00 -0700

    Initial Files
    Things are working for the most part.
     - added materialize-css
     - added home and navbar components

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['../../../node_modules/materialize-css/dist/css/materialize.min.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
