306e08727c6ca53cf1df33851ad2ff8cc5f9f618
Rafael Zasas <zasas.rafi@gmail.com>
2020-06-18 16:52:42 -0700

    Added Feedback Feature
    Has linkage to Cloud Firesotre on firebase

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import M from 'materialize-css';
+import {AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection} from '@angular/fire/firestore';
+import {Observable, observable} from 'rxjs';
+import {AngularFireStorage} from '@angular/fire/storage';
+import {JsonpInterceptor} from '@angular/common/http';
+import {ajaxGetJSON} from 'rxjs/internal-compatibility';
+
+interface Feedback {
+  message: string;
+  type: string;
+}
 
 @Component({
   selector: 'app-feedback',
@@ -7,9 +19,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedbackComponent implements OnInit {
 
-  constructor() { }
+  feedbackCollection: AngularFirestoreCollection<Feedback>; // passing the interface : Feedback
+  feedbackMessages: Observable<Feedback[]>;
+  feedbackForm: FormGroup;
+  options = {};
+  elems: any;
+  btnSubmit = false;
+
+  constructor(private afs: AngularFirestore) {
+  }
 
   ngOnInit(): void {
+    this.feedbackForm = new FormGroup({
+      feedbackMessage: new FormControl('', [Validators.required,
+        Validators.minLength(3)]),
+      feedbackType: new FormControl('General Feedback', Validators.required)
+    }); // init the form
+
+    this.elems = document.querySelectorAll('select');
+    M.FormSelect.init(this.elems, this.options); // for the dropdown menu
+
+    this.feedbackCollection = this.afs.collection('Feedback', ref => { // collection to store firestore data
+      return ref.limit(3);
+    }); // reference
+    this.feedbackMessages = this.feedbackCollection.valueChanges(); // observable of notes data
   }
+
+  onSubmit() {
+    if (this.feedbackForm.valid) {
+      const formData = {
+        feedbackType: this.feedbackForm.value.feedbackType,
+        feedbackMessage: this.feedbackForm.value.feedbackMessage
+      };
+      // @ts-ignore
+      this.feedbackCollection.add(formData).then(r => console.log('stored feedback successfully'),
+        M.toast({html: 'Thank you !', classes: 'rounded blue'}));
+    } else {
+      M.toast({html: 'Please enter a message before submitting.', classes: 'rounded red'});
+    }
+    }
 
 }
