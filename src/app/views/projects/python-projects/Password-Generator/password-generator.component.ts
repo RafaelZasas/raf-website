fd225305599000d4eb13247dd56e6aaab02d5286
Rafael Zasas <zasas.rafi@gmail.com>
2020-12-28 19:46:35 +0200

    Fix Password Generator

--- src/app/views/projects/python-projects/Password-Generator/password-generator.component.ts
+++ src/app/views/projects/python-projects/Password-Generator/password-generator.component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit} from '@angular/core';
+import {AngularFireFunctions} from '@angular/fire/functions';
 import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import M from 'materialize-css';
 import {CustomValidator} from '../../../../form-validators/authentication.validator';
+import {first} from 'rxjs/operators';
 
 
 @Component({
@@ -13,7 +15,6 @@ import {CustomValidator} from '../../../../form-validators/authentication.valida
 export class PasswordGeneratorComponent implements OnInit {
 
 
-
   elems: any;
   password: any;
   userClicked = false;
@@ -22,7 +23,8 @@ export class PasswordGeneratorComponent implements OnInit {
 
   constructor(
     private http: HttpClient,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private fns: AngularFireFunctions
   ) {
   }
 
@@ -30,7 +32,9 @@ export class PasswordGeneratorComponent implements OnInit {
     // INITIALIZE THE FORM GROUP
     this.passwordForm = this.fb.group({
       pwdlength: new FormControl('10', [Validators.required]),
-      useSymbols: new FormControl('true', [Validators.required])
+      useSymbols: new FormControl('true', [Validators.required]),
+      useLetters: new FormControl('true', [Validators.required]),
+      useNumbers: new FormControl('true', [Validators.required]),
     });
 
 
@@ -38,9 +42,29 @@ export class PasswordGeneratorComponent implements OnInit {
     M.FormSelect.init(this.elems); // for the dropdown menu
   }
 
+   getRandomPassword() {
+    this.userClicked = true;  // display the password when clicked
+    // CHECK IF THE FORM HAS BEEN FILLED OUT CORRECTLY
+    if (this.passwordForm.valid) { // check if the form is valid
+      const callable = this.fns.httpsCallable('getRandomPassword');
+      callable({
+        useSymbols: this.getUseSymbols.value,
+        useNumbers: this.getUseNumbers.value,
+        useLetters: this.getUseLetters.value,
+        pwLength: this.pwLength.value
+      }).subscribe(res => {
+        this.password = res.password;
+        console.log(this.password);
+      });
+      // if the user tries entering nothing
+    } else {
+      M.toast({html: 'Please fill entire form before submitting.', classes: 'rounded red'});
+    }
+  }
+
   getPassword() {
     // FOR WHEN THE USER CLICKS THE SUBMIT -> DISPLAY DATA
-    // FOR WHEN THE USER CLICKS THE SUBMIT -> DISPLAY DATA
+
 
     this.userClicked = true;  // display the password when clicked
 
@@ -74,7 +98,6 @@ export class PasswordGeneratorComponent implements OnInit {
   }
 
 
-
   // functions to retrieve form input fields.
   // note* This retrieves the field not the value
   get pwLength() {
@@ -83,6 +106,14 @@ export class PasswordGeneratorComponent implements OnInit {
 
   get getUseSymbols() {
     return this.passwordForm.get('useSymbols');
+  }
+
+  get getUseNumbers() {
+    return this.passwordForm.get('useNumbers');
+  }
+
+  get getUseLetters() {
+    return this.passwordForm.get('useLetters');
   }
 
 
